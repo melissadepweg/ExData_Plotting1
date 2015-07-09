93ffa7906e4b261a24d69764a095d22e6e1bb687
melissadepweg <melissa.depweg@gmail.com>
2015-07-09 08:51:29 -0700

    Updated with comments

--- plot3.R
+++ plot3.R
@@ -1,5 +1,5 @@
 setwd("C:/Users/mdepweg/Documents/coursera/Exploratory Data Analysis")
-
+#Pulling Data
 mydata <- read.table("household_power_consumption.txt", sep=";", 
                      col.names=c("Date", "Time", "Global_active_power","Global_reactive_power", "Voltage", 
                                  "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
@@ -11,6 +11,7 @@ subset <- subset(mydata, Date2>='2007-02-01')
 subset <- subset(subset, Date2<='2007-02-02')
 subset$DateTime<- as.POSIXct(paste(subset$Date2, subset$Time), format="%Y-%m-%d %H:%M:%S")
 
+#Plotting Plot 3
 png(filename="plot3.png", width = 480, height = 480, units = "px")
 plot(subset$DateTime,subset$Sub_metering_1,type="l",col="black", xlab="", ylab="Energy sub metering",main="")
 lines(subset$DateTime,subset$Sub_metering_2,col="red")
