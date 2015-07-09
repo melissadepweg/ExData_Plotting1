9c08812187210e7f7c5f788940695e7d8538d32a
melissadepweg <melissa.depweg@gmail.com>
2015-07-09 08:50:48 -0700

    Updated with comments

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
 setwd("C:/Users/mdepweg/Documents/coursera/Exploratory Data Analysis")
-
+#Pulling Data
 mydata <- read.table("household_power_consumption.txt", sep=";", 
                      col.names=c("Date", "Time", "Global_active_power","Global_reactive_power", "Voltage", 
                                  "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
@@ -12,6 +12,7 @@ subset <- subset(subset, Date2<='2007-02-02')
 subset$DateTime<- as.POSIXct(paste(subset$Date2, subset$Time), format="%Y-%m-%d %H:%M:%S")
 subset$Days <- weekdays(subset$DateTime)
 
+#Plotting Plot2
 png(filename="plot2.png", width = 480, height = 480, units = "px")
 plot(subset$DateTime,subset$Global_active_power,type = "l",xlab="",ylab="Global Active Power (kilowatts)",main="")
 dev.off()
